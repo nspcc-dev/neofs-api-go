6d71ea239bb729a8021a39311b1bd790b0331534
Leonard Lyubich <leonard@nspcc.ru>
2020-05-08 11:55:19 +0300

    session: implement SignedDataSource on CreateRequest

--- session/request.go
+++ session/request.go
@@ -1,5 +1,19 @@
 package session
 
+import (
+	"encoding/binary"
+	"io"
+
+	"github.com/nspcc-dev/neofs-api-go/refs"
+)
+
+const signedRequestDataSize = 0 +
+	refs.OwnerIDSize +
+	8 +
+	8
+
+var requestEndianness = binary.BigEndian
+
 // GetOwnerID is an OwnerID field getter.
 func (m CreateRequest) GetOwnerID() OwnerID {
 	return m.OwnerID
@@ -8,4 +22,37 @@ func (m CreateRequest) GetOwnerID() OwnerID {
 // SetOwnerID is an OwnerID field setter.
 func (m *CreateRequest) SetOwnerID(id OwnerID) {
 	m.OwnerID = id
+}
+
+func (m CreateRequest) SignedData() ([]byte, error) {
+	data := make([]byte, m.SignedDataSize())
+
+	_, err := m.ReadSignedData(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (m CreateRequest) SignedDataSize() int {
+	return signedRequestDataSize
+}
+
+func (m CreateRequest) ReadSignedData(p []byte) (int, error) {
+	sz := m.SignedDataSize()
+	if len(p) < sz {
+		return 0, io.ErrUnexpectedEOF
+	}
+
+	var off int
+
+	off += copy(p[off:], m.GetOwnerID().Bytes())
+
+	requestEndianness.PutUint64(p[off:], m.CreationEpoch())
+	off += 8
+
+	requestEndianness.PutUint64(p[off:], m.ExpirationEpoch())
+
+	return sz, nil
 }
