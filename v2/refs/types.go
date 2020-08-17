1b262fc07247a5e31321be2355006fc6c94f757d
Leonard Lyubich <leonard@nspcc.ru>
2020-08-17 12:03:52 +0300

    v2/refs: Implement remaining uni-structures
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ v2/refs/types.go
@@ -0,0 +1,89 @@
+package refs
+
+type OwnerID struct {
+	val []byte
+}
+
+type ContainerID struct {
+	val []byte
+}
+
+type ObjectID struct {
+	val []byte
+}
+
+type Address struct {
+	cid *ContainerID
+
+	oid *ObjectID
+}
+
+func (o *OwnerID) GetValue() []byte {
+	if o != nil {
+		return o.val
+	}
+
+	return nil
+}
+
+func (o *OwnerID) SetValue(v []byte) {
+	if o != nil {
+		o.val = v
+	}
+}
+
+func (o *ContainerID) GetValue() []byte {
+	if o != nil {
+		return o.val
+	}
+
+	return nil
+}
+
+func (o *ContainerID) SetValue(v []byte) {
+	if o != nil {
+		o.val = v
+	}
+}
+
+func (o *ObjectID) GetValue() []byte {
+	if o != nil {
+		return o.val
+	}
+
+	return nil
+}
+
+func (o *ObjectID) SetValue(v []byte) {
+	if o != nil {
+		o.val = v
+	}
+}
+
+func (a *Address) GetContainerID() *ContainerID {
+	if a != nil {
+		return a.cid
+	}
+
+	return nil
+}
+
+func (a *Address) SetContainerID(v *ContainerID) {
+	if a != nil {
+		a.cid = v
+	}
+}
+
+func (a *Address) GetObjectID() *ObjectID {
+	if a != nil {
+		return a.oid
+	}
+
+	return nil
+}
+
+func (a *Address) SetObjectID(v *ObjectID) {
+	if a != nil {
+		a.oid = v
+	}
+}
