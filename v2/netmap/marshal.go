82110751e75799740e89355d11e75ac4eea39c62
Alex Vanin <alexey@nspcc.ru>
2020-08-18 16:48:58 +0300

    Add stable marshal for container package
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

---
+++ v2/netmap/marshal.go
@@ -0,0 +1,11 @@
+package netmap
+
+func (p *PlacementPolicy) StableMarshal(buf []byte) ([]byte, error) {
+	// todo: implement me
+	return nil, nil
+}
+
+func (p *PlacementPolicy) StableSize() (size int) {
+	// todo: implement me
+	return 0
+}
