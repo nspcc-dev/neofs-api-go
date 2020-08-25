86e6221b7632c567b46a189947c83253d18a918e
Alex Vanin <alexey@nspcc.ru>
2020-08-25 14:51:00 +0300

    [#126] sdk: add sdk client for all available api requests
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

---
+++ pkg/accounting/decimal.go
@@ -0,0 +1,9 @@
+package accounting
+
+import (
+	"github.com/nspcc-dev/neofs-api-go/v2/accounting"
+)
+
+type Decimal struct {
+	accounting.Decimal
+}
---
+++ pkg/client/client.go
@@ -0,0 +1,50 @@
+package client
+
+import (
+	"crypto/ecdsa"
+	"errors"
+
+	"github.com/nspcc-dev/neofs-api-go/pkg"
+)
+
+type (
+	Client struct {
+		key        *ecdsa.PrivateKey
+		remoteNode TransportInfo
+
+		opts *clientOptions
+	}
+
+	TransportProtocol uint32
+
+	TransportInfo struct {
+		Version  pkg.Version
+		Protocol TransportProtocol
+	}
+)
+
+const (
+	Unknown TransportProtocol = iota
+	GRPC
+)
+
+var (
+	unsupportedProtocolErr = errors.New("unsupported transport protocol")
+)
+
+func New(key *ecdsa.PrivateKey, opts ...ClientOption) (*Client, error) {
+	clientOptions := defaultClientOptions()
+	for i := range opts {
+		opts[i].apply(clientOptions)
+	}
+
+	// todo: make handshake to check latest version
+	return &Client{
+		key: key,
+		remoteNode: TransportInfo{
+			Version:  pkg.SDKVersion,
+			Protocol: GRPC,
+		},
+		opts: clientOptions,
+	}, nil
+}
