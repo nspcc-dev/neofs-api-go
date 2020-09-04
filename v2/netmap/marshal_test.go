83bb40d282ad2662944544ba643dcf8867cda4e6
Alex Vanin <alexey@nspcc.ru>
2020-09-04 13:05:56 +0300

    [#134] v2/netmap: Update unified structures
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- v2/netmap/marshal_test.go
+++ v2/netmap/marshal_test.go
@@ -25,7 +25,7 @@ func TestAttribute_StableMarshal(t *testing.T) {
 	})
 }
 
-func TestNodeInfo(t *testing.T) {
+func TestNodeInfo_StableMarshal(t *testing.T) {
 	from := generateNodeInfo("publicKey", "/multi/addr", 10)
 	transport := new(grpc.NodeInfo)
 
@@ -41,10 +41,75 @@ func TestNodeInfo(t *testing.T) {
 	})
 }
 
+func TestFilter_StableMarshal(t *testing.T) {
+	from := generateFilter("key", "value", false)
+	transport := new(grpc.Filter)
+
+	t.Run("non empty", func(t *testing.T) {
+		wire, err := from.StableMarshal(nil)
+		require.NoError(t, err)
+
+		err = transport.Unmarshal(wire)
+		require.NoError(t, err)
+
+		to := netmap.FilterFromGRPCMessage(transport)
+		require.Equal(t, from, to)
+	})
+}
+
+func TestSelector_StableMarshal(t *testing.T) {
+	from := generateSelector("name")
+	transport := new(grpc.Selector)
+
+	t.Run("non empty", func(t *testing.T) {
+		wire, err := from.StableMarshal(nil)
+		require.NoError(t, err)
+
+		err = transport.Unmarshal(wire)
+		require.NoError(t, err)
+
+		to := netmap.SelectorFromGRPCMessage(transport)
+		require.Equal(t, from, to)
+	})
+}
+
+func TestReplica_StableMarshal(t *testing.T) {
+	from := generateReplica("selector")
+	transport := new(grpc.Replica)
+
+	t.Run("non empty", func(t *testing.T) {
+		wire, err := from.StableMarshal(nil)
+		require.NoError(t, err)
+
+		err = transport.Unmarshal(wire)
+		require.NoError(t, err)
+
+		to := netmap.ReplicaFromGRPCMessage(transport)
+		require.Equal(t, from, to)
+	})
+}
+
+func TestPlacementPolicy_StableSize(t *testing.T) {
+	from := generatePolicy(3)
+	transport := new(grpc.PlacementPolicy)
+
+	t.Run("non empty", func(t *testing.T) {
+		wire, err := from.StableMarshal(nil)
+		require.NoError(t, err)
+
+		err = transport.Unmarshal(wire)
+		require.NoError(t, err)
+
+		to := netmap.PlacementPolicyFromGRPCMessage(transport)
+		require.Equal(t, from, to)
+	})
+}
+
 func generateAttribute(k, v string) *netmap.Attribute {
 	attr := new(netmap.Attribute)
 	attr.SetKey(k)
 	attr.SetValue(v)
+	attr.SetParents([]string{k, v})
 
 	return attr
 }
@@ -53,6 +118,7 @@ func generateNodeInfo(key, addr string, n int) *netmap.NodeInfo {
 	nodeInfo := new(netmap.NodeInfo)
 	nodeInfo.SetPublicKey([]byte(key))
 	nodeInfo.SetAddress(addr)
+	nodeInfo.SetState(netmap.Online)
 
 	attrs := make([]*netmap.Attribute, n)
 	for i := 0; i < n; i++ {
@@ -63,4 +129,62 @@ func generateNodeInfo(key, addr string, n int) *netmap.NodeInfo {
 	nodeInfo.SetAttributes(attrs)
 
 	return nodeInfo
+}
+
+func generateFilter(key, value string, fin bool) *netmap.Filter {
+	f := new(netmap.Filter)
+	f.SetKey(key)
+	f.SetValue(value)
+	f.SetName("name")
+	f.SetOp(netmap.AND)
+	if !fin {
+		ff := generateFilter(key+"fin", value+"fin", true)
+		f.SetFilters([]*netmap.Filter{ff})
+	} else {
+		f.SetFilters([]*netmap.Filter{})
+	}
+
+	return f
+}
+
+func generateSelector(name string) *netmap.Selector {
+	s := new(netmap.Selector)
+	s.SetName(name)
+	s.SetAttribute("attribute")
+	s.SetCount(10)
+	s.SetFilter("filter")
+
+	return s
+}
+
+func generateReplica(selector string) *netmap.Replica {
+	r := new(netmap.Replica)
+	r.SetCount(10)
+	r.SetSelector(selector)
+
+	return r
+}
+
+func generatePolicy(n int) *netmap.PlacementPolicy {
+	var (
+		p = new(netmap.PlacementPolicy)
+		f = make([]*netmap.Filter, 0, n)
+		s = make([]*netmap.Selector, 0, n)
+		r = make([]*netmap.Replica, 0, n)
+	)
+
+	for i := 0; i < n; i++ {
+		ind := strconv.Itoa(i)
+
+		f = append(f, generateFilter("key"+ind, "val"+ind, false))
+		s = append(s, generateSelector("name"+ind))
+		r = append(r, generateReplica("selector"+ind))
+	}
+
+	p.SetFilters(f)
+	p.SetSelectors(s)
+	p.SetReplicas(r)
+	p.SetContainerBackupFactor(10)
+
+	return p
 }
--- v2/netmap/types.go
+++ v2/netmap/types.go
@@ -1,13 +1,39 @@
 package netmap
 
+type Filter struct {
+	name    string
+	key     string
+	op      Operation
+	value   string
+	filters []*Filter
+}
+
+type Selector struct {
+	name      string
+	count     uint32
+	attribute string
+	filter    string
+}
+
+type Replica struct {
+	count    uint32
+	selector string
+}
+
+type Operation uint32
+
 type PlacementPolicy struct {
-	// TODO: fill me
+	replicas     []*Replica
+	backupFactor uint32
+	selectors    []*Selector
+	filters      []*Filter
 }
 
 // Attribute of storage node.
 type Attribute struct {
-	key   string
-	value string
+	key     string
+	value   string
+	parents []string
 }
 
 // NodeInfo of storage node.
@@ -15,16 +41,233 @@ type NodeInfo struct {
 	publicKey  []byte
 	address    string
 	attributes []*Attribute
+	state      NodeState
 }
 
 // NodeState of storage node.
 type NodeState uint32
 
 const (
-	Unspecified NodeState = iota
+	UnspecifiedState NodeState = iota
 	Online
 	Offline
 )
+
+const (
+	UnspecifiedOperation Operation = iota
+	EQ
+	NE
+	GT
+	GE
+	LT
+	LE
+	OR
+	AND
+)
+
+func (f *Filter) GetFilters() []*Filter {
+	if f != nil {
+		return f.filters
+	}
+
+	return nil
+}
+
+func (f *Filter) SetFilters(filters []*Filter) {
+	if f != nil {
+		f.filters = filters
+	}
+}
+
+func (f *Filter) GetValue() string {
+	if f != nil {
+		return f.value
+	}
+
+	return ""
+}
+
+func (f *Filter) SetValue(value string) {
+	if f != nil {
+		f.value = value
+	}
+}
+
+func (f *Filter) GetOp() Operation {
+	if f != nil {
+		return f.op
+	}
+	return UnspecifiedOperation
+}
+
+func (f *Filter) SetOp(op Operation) {
+	if f != nil {
+		f.op = op
+	}
+}
+
+func (f *Filter) GetKey() string {
+	if f != nil {
+		return f.key
+	}
+
+	return ""
+}
+
+func (f *Filter) SetKey(key string) {
+	if f != nil {
+		f.key = key
+	}
+}
+
+func (f *Filter) GetName() string {
+	if f != nil {
+		return f.name
+	}
+
+	return ""
+}
+
+func (f *Filter) SetName(name string) {
+	if f != nil {
+		f.name = name
+	}
+}
+
+func (s *Selector) GetFilter() string {
+	if s != nil {
+		return s.filter
+	}
+
+	return ""
+}
+
+func (s *Selector) SetFilter(filter string) {
+	if s != nil {
+		s.filter = filter
+	}
+}
+
+func (s *Selector) GetAttribute() string {
+	if s != nil {
+		return s.attribute
+	}
+
+	return ""
+}
+
+func (s *Selector) SetAttribute(attribute string) {
+	if s != nil {
+		s.attribute = attribute
+	}
+}
+
+func (s *Selector) GetCount() uint32 {
+	if s != nil {
+		return s.count
+	}
+
+	return 0
+}
+
+func (s *Selector) SetCount(count uint32) {
+	if s != nil {
+		s.count = count
+	}
+}
+
+func (s *Selector) GetName() string {
+	if s != nil {
+		return s.name
+	}
+
+	return ""
+}
+
+func (s *Selector) SetName(name string) {
+	if s != nil {
+		s.name = name
+	}
+}
+
+func (r *Replica) GetSelector() string {
+	if r != nil {
+		return r.selector
+	}
+
+	return ""
+}
+
+func (r *Replica) SetSelector(selector string) {
+	if r != nil {
+		r.selector = selector
+	}
+}
+
+func (r *Replica) GetCount() uint32 {
+	if r != nil {
+		return r.count
+	}
+
+	return 0
+}
+
+func (r *Replica) SetCount(count uint32) {
+	if r != nil {
+		r.count = count
+	}
+}
+
+func (p *PlacementPolicy) GetFilters() []*Filter {
+	if p != nil {
+		return p.filters
+	}
+
+	return nil
+}
+
+func (p *PlacementPolicy) SetFilters(filters []*Filter) {
+	if p != nil {
+
+	}
+	p.filters = filters
+}
+
+func (p *PlacementPolicy) GetSelectors() []*Selector {
+	if p != nil {
+		return p.selectors
+	}
+
+	return nil
+}
+
+func (p *PlacementPolicy) SetSelectors(selectors []*Selector) {
+	if p != nil {
+		p.selectors = selectors
+	}
+}
+
+func (p *PlacementPolicy) GetContainerBackupFactor() uint32 {
+	if p != nil {
+		return p.backupFactor
+	}
+
+	return 0
+}
+
+func (p *PlacementPolicy) SetContainerBackupFactor(backupFactor uint32) {
+	if p != nil {
+		p.backupFactor = backupFactor
+	}
+}
+
+func (p *PlacementPolicy) GetReplicas() []*Replica {
+	return p.replicas
+}
+
+func (p *PlacementPolicy) SetReplicas(replicas []*Replica) {
+	p.replicas = replicas
+}
 
 func (a *Attribute) GetKey() string {
 	if a != nil {
@@ -51,6 +294,20 @@ func (a *Attribute) GetValue() string {
 func (a *Attribute) SetValue(v string) {
 	if a != nil {
 		a.value = v
+	}
+}
+
+func (a *Attribute) GetParents() []string {
+	if a != nil {
+		return a.parents
+	}
+
+	return nil
+}
+
+func (a *Attribute) SetParents(parent []string) {
+	if a != nil {
+		a.parents = parent
 	}
 }
 
@@ -93,5 +350,19 @@ func (ni *NodeInfo) GetAttributes() []*Attribute {
 func (ni *NodeInfo) SetAttributes(v []*Attribute) {
 	if ni != nil {
 		ni.attributes = v
+	}
+}
+
+func (ni *NodeInfo) GetState() NodeState {
+	if ni != nil {
+		return ni.state
+	}
+
+	return UnspecifiedState
+}
+
+func (ni *NodeInfo) SetState(state NodeState) {
+	if ni != nil {
+		ni.state = state
 	}
 }
