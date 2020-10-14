2ff00cda043bae3d9dcaba4f7f24aeaceb34a4ac
Evgeniy Kulikov <kim@nspcc.ru>
2020-10-14 22:09:13 +0300

    [#147] Work around owner package
    Add NEO3WalletSize as constant value.
    
    closes #147
    
    Signed-off-by: Evgeniy Kulikov <kim@nspcc.ru>

--- pkg/owner/wallet.go
+++ pkg/owner/wallet.go
@@ -10,7 +10,10 @@ import (
 )
 
 // NEO3Wallet represents NEO3 wallet address.
-type NEO3Wallet [25]byte
+type NEO3Wallet [NEO3WalletSize]byte
+
+// NEO3WalletSize contains size of neo3 wallet.
+const NEO3WalletSize = 25
 
 // NEO3WalletFromPublicKey converts public key to NEO3 wallet address.
 func NEO3WalletFromPublicKey(key *ecdsa.PublicKey) (*NEO3Wallet, error) {
