20273357f68162d661574460a7fef29eabcaa141
Evgeniy Kulikov <kim@nspcc.ru>
2020-10-14 22:08:27 +0300

    [#147] Work around object package
    - implement Stringer interface and Parse method for object.Address
    - increase test coverage
    
    closes #147
    
    Signed-off-by: Evgeniy Kulikov <kim@nspcc.ru>

--- pkg/object/address_test.go
+++ pkg/object/address_test.go
@@ -1,6 +1,7 @@
 package object
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/nspcc-dev/neofs-api-go/pkg/container"
@@ -26,4 +27,36 @@ func TestAddress_SetObjectID(t *testing.T) {
 	a.SetObjectID(oid)
 
 	require.Equal(t, oid, a.GetObjectID())
+}
+
+func TestAddress_Parse(t *testing.T) {
+	cid := container.NewID()
+	cid.SetSHA256(randSHA256Checksum(t))
+
+	oid := NewID()
+	oid.SetSHA256(randSHA256Checksum(t))
+
+	t.Run("should parse successful", func(t *testing.T) {
+		s := strings.Join([]string{cid.String(), oid.String()}, addressSeparator)
+		a := NewAddress()
+
+		require.NoError(t, a.Parse(s))
+		require.Equal(t, oid, a.GetObjectID())
+		require.Equal(t, cid, a.GetContainerID())
+	})
+
+	t.Run("should fail for bad address", func(t *testing.T) {
+		s := strings.Join([]string{cid.String()}, addressSeparator)
+		require.EqualError(t, NewAddress().Parse(s), ErrBadAddress.Error())
+	})
+
+	t.Run("should fail on container.ID", func(t *testing.T) {
+		s := strings.Join([]string{"1", "2"}, addressSeparator)
+		require.EqualError(t, NewAddress().Parse(s), container.ErrBadID.Error())
+	})
+
+	t.Run("should fail on object.ID", func(t *testing.T) {
+		s := strings.Join([]string{cid.String(), "2"}, addressSeparator)
+		require.EqualError(t, NewAddress().Parse(s), ErrBadID.Error())
+	})
 }
