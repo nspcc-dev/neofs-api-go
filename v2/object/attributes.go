dcd573ef2840250b25b6be39f6da017b778960c9
Leonard Lyubich <leonard@nspcc.ru>
2020-10-22 19:10:42 +0300

    [#172] Improve the readability of named attribute constants
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- v2/object/attributes.go
+++ v2/object/attributes.go
@@ -1,12 +1,12 @@
 package object
 
-// SysAttrPrefix is a prefix of key to system attribute.
-const SysAttrPrefix = "__NEOFS__"
+// SysAttributePrefix is a prefix of key to system attribute.
+const SysAttributePrefix = "__NEOFS__"
 
 const (
-	// SysAttrUploadID marks smaller parts of a split bigger object.
-	SysAttrUploadID = SysAttrPrefix + "UPLOAD_ID"
+	// SysAttributeUploadID marks smaller parts of a split bigger object.
+	SysAttributeUploadID = SysAttributePrefix + "UPLOAD_ID"
 
-	// SysAttrExpEpoch tells GC to delete object after that epoch.
-	SysAttrExpEpoch = SysAttrPrefix + "EXPIRATION_EPOCH"
+	// SysAttributeExpEpoch tells GC to delete object after that epoch.
+	SysAttributeExpEpoch = SysAttributePrefix + "EXPIRATION_EPOCH"
 )
