101714285ad2ba18a3be81335e14738ac5262254
Leonard Lyubich <leonard@nspcc.ru>
2020-10-22 18:41:34 +0300

    [#172] sdk/container: Define well-known application attributes
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/container/wellknown_attributes.go
@@ -0,0 +1,11 @@
+package container
+
+const (
+	// AttrName is an attribute key that is commonly used to denote
+	// human-friendly name.
+	AttrName = "Name"
+
+	// AttrTimestamp is an attribute key that is commonly used to denote
+	// user-defined local time of container creation in Unix Timestamp format.
+	AttrTimestamp = "Timestamp"
+)
