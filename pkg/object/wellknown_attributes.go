5494d3417f8360b46c776188c65c276f633ae71b
Leonard Lyubich <leonard@nspcc.ru>
2020-10-22 18:41:49 +0300

    [#172] sdk/object: Define well-known application attributes
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/object/wellknown_attributes.go
@@ -0,0 +1,15 @@
+package object
+
+const (
+	// AttrName is an attribute key that is commonly used to denote
+	// human-friendly name.
+	AttrName = "Name"
+
+	// AttrFileName is an attribute key that is commonly used to denote
+	// file name to be associated with the object on saving.
+	AttrFileName = "FileName"
+
+	// AttrTimestamp is an attribute key that is commonly used to denote
+	// user-defined local time of object creation in Unix Timestamp format.
+	AttrTimestamp = "Timestamp"
+)
