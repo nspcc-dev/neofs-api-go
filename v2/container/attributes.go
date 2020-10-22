56802b8e5cea3b5f3a8cc05e2221afc91a75b4f8
Leonard Lyubich <leonard@nspcc.ru>
2020-10-22 18:40:55 +0300

    [#172] v2/container: Define well-known system attributes
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ v2/container/attributes.go
@@ -0,0 +1,9 @@
+package container
+
+// SysAttrPrefix is a prefix of key to system attribute.
+const SysAttrPrefix = "__NEOFS__"
+
+const (
+	// SysAttrSubnet is a string ID of container's storage subnet.
+	SysAttrSubnet = SysAttrPrefix + "SUBNET"
+)
