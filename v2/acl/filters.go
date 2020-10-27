efaa9b443fc97978ea416e6b475c1404ba02c184
Leonard Lyubich <leonard@nspcc.ru>
2020-10-27 18:13:14 +0300

    [#182] v2/acl: Define filter keys for object headers
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ v2/acl/filters.go
@@ -0,0 +1,33 @@
+package acl
+
+// ObjectFilterPrefix is a prefix of key to object header value or property.
+const ObjectFilterPrefix = "$Object:"
+
+const (
+	// FilterObjectVersion is a filter key to "version" field of the object header.
+	FilterObjectVersion = ObjectFilterPrefix + "version"
+
+	// FilterObjectContainerID is a filter key to "container_id" field of the object header.
+	FilterObjectContainerID = ObjectFilterPrefix + "containerID"
+
+	// FilterObjectOwnerID is a filter key to "owner_id" field of the object header.
+	FilterObjectOwnerID = ObjectFilterPrefix + "ownerID"
+
+	// FilterObjectCreationEpoch is a filter key to "creation_epoch" field of the object header.
+	FilterObjectCreationEpoch = ObjectFilterPrefix + "creationEpoch"
+
+	// FilterObjectPayloadLength is a filter key to "payload_length" field of the object header.
+	FilterObjectPayloadLength = ObjectFilterPrefix + "payloadLength"
+
+	// FilterObjectPayloadHash is a filter key to "payload_hash" field of the object header.
+	FilterObjectPayloadHash = ObjectFilterPrefix + "payloadHash"
+
+	// FilterObjectType is a filter key to "object_type" field of the object header.
+	FilterObjectType = ObjectFilterPrefix + "objectType"
+
+	// FilterObjectHomomorphicHash is a filter key to "homomorphic_hash" field of the object header.
+	FilterObjectHomomorphicHash = ObjectFilterPrefix + "homomorphicHash"
+
+	// FilterObjectParent is a filter key to "split.parent" field of the object header.
+	FilterObjectParent = ObjectFilterPrefix + "split.parent"
+)
