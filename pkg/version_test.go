b27bbd9b12dec1915ca23584e3bd0c8cb72558a0
Leonard Lyubich <leonard@nspcc.ru>
2020-11-03 11:29:41 +0300

    [#187] sdk: Implement IsSupportedVersion function
    Implement function that checks the compatibility of the specified version
    with the current NeoFS revision.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/version_test.go
+++ pkg/version_test.go
@@ -29,3 +29,36 @@ func TestSDKVersion(t *testing.T) {
 	require.Equal(t, uint32(sdkMjr), v.GetMajor())
 	require.Equal(t, uint32(sdkMnr), v.GetMinor())
 }
+
+func TestIsSupportedVersion(t *testing.T) {
+	require.Error(t, IsSupportedVersion(nil))
+
+	v := NewVersion()
+
+	v.SetMajor(1)
+	require.Error(t, IsSupportedVersion(v))
+
+	v.SetMajor(3)
+	require.Error(t, IsSupportedVersion(v))
+
+	for _, item := range []struct {
+		mjr, maxMnr uint32
+	}{
+		{
+			mjr:    2,
+			maxMnr: 0,
+		},
+	} {
+		v.SetMajor(item.mjr)
+
+		for i := uint32(0); i < item.maxMnr; i++ {
+			v.SetMinor(i)
+
+			require.NoError(t, IsSupportedVersion(v))
+		}
+
+		v.SetMinor(item.maxMnr + 1)
+
+		require.Error(t, IsSupportedVersion(v))
+	}
+}
