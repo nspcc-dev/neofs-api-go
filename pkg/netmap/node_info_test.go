6347f846ebff0b1cc49b099d47939973681b0c8b
Leonard Lyubich <leonard@nspcc.ru>
2020-11-05 14:39:50 +0300

    [#189] sdk/netmap: Implement NodeAttribute type
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/netmap/node_info_test.go
+++ pkg/netmap/node_info_test.go
@@ -42,3 +42,41 @@ func TestNodeStateFromV2(t *testing.T) {
 		require.Equal(t, item.sV2, item.s.ToV2())
 	}
 }
+
+func TestNodeAttributeFromV2(t *testing.T) {
+	aV2 := new(netmap.Attribute)
+	aV2.SetKey("key")
+	aV2.SetValue("value")
+	aV2.SetParents([]string{"par1", "par2"})
+
+	a := NewNodeAttributeFromV2(aV2)
+
+	require.Equal(t, aV2, a.ToV2())
+}
+
+func TestNodeAttribute_Key(t *testing.T) {
+	a := NewNodeAttribute()
+	key := "some key"
+
+	a.SetKey(key)
+
+	require.Equal(t, key, a.Key())
+}
+
+func TestNodeAttribute_Value(t *testing.T) {
+	a := NewNodeAttribute()
+	val := "some value"
+
+	a.SetValue(val)
+
+	require.Equal(t, val, a.Value())
+}
+
+func TestNodeAttribute_ParentKeys(t *testing.T) {
+	a := NewNodeAttribute()
+	keys := []string{"par1", "par2"}
+
+	a.SetParentKeys(keys...)
+
+	require.Equal(t, keys, a.ParentKeys())
+}
