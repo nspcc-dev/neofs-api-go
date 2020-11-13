17c32b6f50a0e69f457526b910202ae1c45f51ab
Leonard Lyubich <leonard@nspcc.ru>
2020-11-13 13:36:18 +0300

    [#168] sdk/accounting: Refactor Decimal type
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/accounting/decimal.go
+++ pkg/accounting/decimal.go
@@ -4,6 +4,39 @@ import (
 	"github.com/nspcc-dev/neofs-api-go/v2/accounting"
 )
 
-type Decimal struct {
-	accounting.Decimal
+// Decimal represents v2-compatible decimal number.
+type Decimal accounting.Decimal
+
+// NewDecimal creates, initializes and returns blank Decimal instance.
+func NewDecimal() *Decimal {
+	return NewDecimalFromV2(new(accounting.Decimal))
+}
+
+// NewDecimalFromV2 converts v2 Decimal to Decimal.
+func NewDecimalFromV2(d *accounting.Decimal) *Decimal {
+	return (*Decimal)(d)
+}
+
+// Value returns value of the decimal number.
+func (d *Decimal) Value() int64 {
+	return (*accounting.Decimal)(d).
+		GetValue()
+}
+
+// SetValue sets value of the decimal number.
+func (d *Decimal) SetValue(v int64) {
+	(*accounting.Decimal)(d).
+		SetValue(v)
+}
+
+// Precision returns precision of the decimal number.
+func (d *Decimal) Precision() uint32 {
+	return (*accounting.Decimal)(d).
+		GetPrecision()
+}
+
+// SetPrecision sets precision of the decimal number.
+func (d *Decimal) SetPrecision(p uint32) {
+	(*accounting.Decimal)(d).
+		SetPrecision(p)
 }
