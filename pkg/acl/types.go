2b3502d6c0cc9d06befee10519ac0e5ef86d2b75
Leonard Lyubich <leonard@nspcc.ru>
2020-11-16 16:37:40 +0300

    [#199] sdk/acl: Add comments for basic ACL constants
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/acl/types.go
+++ pkg/acl/types.go
@@ -1,7 +1,12 @@
 package acl
 
 const (
-	PublicBasicRule   = 0x1FFFFFFF
-	PrivateBasicRule  = 0x18888888
+	// PublicBasicRule is a basic ACL value for public container.
+	PublicBasicRule = 0x1FFFFFFF
+
+	// PrivateBasicRule is a basic ACL value for private container.
+	PrivateBasicRule = 0x18888888
+
+	// ReadOnlyBasicRule is a basic ACL value for read-only container.
 	ReadOnlyBasicRule = 0x1FFF88FF
 )
