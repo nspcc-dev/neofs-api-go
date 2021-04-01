cefad5956eac39776990e9475321b148fd1bdf6e
Leonard Lyubich <leonard@nspcc.ru>
2021-04-01 17:24:38 +0300

    [#265] reputation/grpc: Implement setters on generated messages
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- v2/reputation/grpc/types.go
+++ v2/reputation/grpc/types.go
@@ -1,7 +1,18 @@
 package reputation
 
+import (
+	refs "github.com/nspcc-dev/neofs-api-go/v2/refs/grpc"
+)
+
+// SetValue sets binary ID.
+func (x *PeerID) SetValue(v []byte) {
+	if x != nil {
+		x.Value = v
+	}
+}
+
 // SetPeer sets trusted peer's ID.
-func (x *Trust) SetPeer(v []byte) {
+func (x *Trust) SetPeer(v *PeerID) {
 	if x != nil {
 		x.Peer = v
 	}
@@ -11,5 +22,40 @@ func (x *Trust) SetPeer(v []byte) {
 func (x *Trust) SetValue(v float64) {
 	if x != nil {
 		x.Value = v
+	}
+}
+
+// SetManager sets manager ID.
+func (x *GlobalTrust_Body) SetManager(v *PeerID) {
+	if x != nil {
+		x.Manager = v
+	}
+}
+
+// SetTrust sets global trust value.
+func (x *GlobalTrust_Body) SetTrust(v *Trust) {
+	if x != nil {
+		x.Trust = v
+	}
+}
+
+// SetVersion sets message format version.
+func (x *GlobalTrust) SetVersion(v *refs.Version) {
+	if x != nil {
+		x.Version = v
+	}
+}
+
+// SetBody sets message body.
+func (x *GlobalTrust) SetBody(v *GlobalTrust_Body) {
+	if x != nil {
+		x.Body = v
+	}
+}
+
+// SetSignature sets body signature.
+func (x *GlobalTrust) SetSignature(v *refs.Signature) {
+	if x != nil {
+		x.Signature = v
 	}
 }
