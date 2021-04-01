ca2e272d42bc779f01a20e6c0f5144b4101adcd2
Leonard Lyubich <leonard@nspcc.ru>
2021-04-01 17:24:38 +0300

    [#265] reputation/grpc: Implement setters on generated messages
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- v2/reputation/grpc/service.go
+++ v2/reputation/grpc/service.go
@@ -59,3 +59,59 @@ func (x *SendLocalTrustResponse) SetVerifyHeader(v *session.ResponseVerification
 		x.VerifyHeader = v
 	}
 }
+
+// SetIteration sets sequence number of the iteration.
+func (x *SendIntermediateResultRequest_Body) SetIteration(v uint32) {
+	if x != nil {
+		x.Iteration = v
+	}
+}
+
+// SetTrust sets current global trust value.
+func (x *SendIntermediateResultRequest_Body) SetTrust(v *Trust) {
+	if x != nil {
+		x.Trust = v
+	}
+}
+
+// SetBody sets body of the request.
+func (x *SendIntermediateResultRequest) SetBody(v *SendIntermediateResultRequest_Body) {
+	if x != nil {
+		x.Body = v
+	}
+}
+
+// SetMetaHeader sets meta header of the request.
+func (x *SendIntermediateResultRequest) SetMetaHeader(v *session.RequestMetaHeader) {
+	if x != nil {
+		x.MetaHeader = v
+	}
+}
+
+// SetVerifyHeader sets verification header of the request.
+func (x *SendIntermediateResultRequest) SetVerifyHeader(v *session.RequestVerificationHeader) {
+	if x != nil {
+		x.VerifyHeader = v
+	}
+}
+
+// SetBody sets body of the response.
+func (x *SendIntermediateResultResponse) SetBody(v *SendIntermediateResultResponse_Body) {
+	if x != nil {
+		x.Body = v
+	}
+}
+
+// SetMetaHeader sets meta header of the response.
+func (x *SendIntermediateResultResponse) SetMetaHeader(v *session.ResponseMetaHeader) {
+	if x != nil {
+		x.MetaHeader = v
+	}
+}
+
+// SetVerifyHeader sets verification header of the response.
+func (x *SendIntermediateResultResponse) SetVerifyHeader(v *session.ResponseVerificationHeader) {
+	if x != nil {
+		x.VerifyHeader = v
+	}
+}
