8c58a7ed12852a7f1bd78b8478fa266e49914c40
Leonard Lyubich <leonard@nspcc.ru>
2021-04-02 12:57:53 +0300

    [#265] pkg/reputation: Implement GlobalTrust type
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/reputation/test/generate.go
+++ pkg/reputation/test/generate.go
@@ -1,9 +1,12 @@
 package reputationtest
 
 import (
+	"testing"
+
 	"github.com/nspcc-dev/neofs-api-go/pkg/reputation"
 	crypto "github.com/nspcc-dev/neofs-crypto"
 	"github.com/nspcc-dev/neofs-crypto/test"
+	"github.com/stretchr/testify/require"
 )
 
 func GeneratePeerID() *reputation.PeerID {
@@ -23,4 +26,20 @@ func GenerateTrust() *reputation.Trust {
 	v.SetValue(1.5)
 
 	return v
+}
+
+func GenerateGlobalTrust() *reputation.GlobalTrust {
+	v := reputation.NewGlobalTrust()
+	v.SetManager(GeneratePeerID())
+	v.SetTrust(GenerateTrust())
+
+	return v
+}
+
+func GenerateSignedGlobalTrust(t testing.TB) *reputation.GlobalTrust {
+	gt := GenerateGlobalTrust()
+
+	require.NoError(t, gt.Sign(test.DecodeKey(0)))
+
+	return gt
 }
