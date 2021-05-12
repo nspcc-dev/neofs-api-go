7671a7463c25af39442f31d6ee9d88e03488f435
Alex Vanin <alexey@nspcc.ru>
2021-05-12 12:41:52 +0300

    Add NFT related container attributes
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- v2/container/attributes.go
+++ v2/container/attributes.go
@@ -7,3 +7,19 @@ const (
 	// SysAttributeSubnet is a string ID of container's storage subnet.
 	SysAttributeSubnet = SysAttributePrefix + "SUBNET"
 )
+
+// NFT related attributes.
+const (
+	// SysAttributeNFTID is a string of token ID associated with the container.
+	SysAttributeNFTID = SysAttributePrefix + "NFT-ID"
+	// SysAttributeNFTAddress is a address of the contract that produced NFT,
+	// encoded in LittleEndian.
+	SysAttributeNFTAddress = SysAttributePrefix + "NFT-Address"
+	// SysAttributeNFTType is a type of NFT. NeoFS supports only `NEP-11` NFT.
+	SysAttributeNFTType = SysAttributePrefix + "NFT-Type"
+	// SysAttributeNFTChain is a NFT blockchain. NeoFS supports only `N3`
+	// main net.
+	SysAttributeNFTChain = SysAttributePrefix + "NFT-Chain"
+	// SysAttributeNFTOptions is an additional data for later use.
+	SysAttributeNFTOptions = SysAttributePrefix + "NFT-Options"
+)
