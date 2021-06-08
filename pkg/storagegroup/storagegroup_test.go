4b216dce58ef9c9e3bde951ee72c0361f627514b
Leonard Lyubich <leonard@nspcc.ru>
2021-06-08 14:40:12 +0300

    [#293] pkg/storagegroup: Implement and use generator of StorageGroup
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- pkg/storagegroup/storagegroup_test.go
+++ pkg/storagegroup/storagegroup_test.go
@@ -1,28 +1,15 @@
 package storagegroup_test
 
 import (
-	"crypto/rand"
-	"crypto/sha256"
 	"testing"
 
-	"github.com/nspcc-dev/neofs-api-go/pkg"
 	"github.com/nspcc-dev/neofs-api-go/pkg/object"
 	objecttest "github.com/nspcc-dev/neofs-api-go/pkg/object/test"
 	"github.com/nspcc-dev/neofs-api-go/pkg/storagegroup"
+	storagegrouptest "github.com/nspcc-dev/neofs-api-go/pkg/storagegroup/test"
+	refstest "github.com/nspcc-dev/neofs-api-go/pkg/test"
 	"github.com/stretchr/testify/require"
 )
-
-func testSHA256() (cs [sha256.Size]byte) {
-	_, _ = rand.Read(cs[:])
-	return
-}
-
-func testChecksum() *pkg.Checksum {
-	h := pkg.NewChecksum()
-	h.SetSHA256(testSHA256())
-
-	return h
-}
 
 func TestStorageGroup(t *testing.T) {
 	sg := storagegroup.New()
@@ -31,7 +18,7 @@ func TestStorageGroup(t *testing.T) {
 	sg.SetValidationDataSize(sz)
 	require.Equal(t, sz, sg.ValidationDataSize())
 
-	cs := testChecksum()
+	cs := refstest.Checksum()
 	sg.SetValidationDataHash(cs)
 	require.Equal(t, cs, sg.ValidationDataHash())
 
@@ -39,17 +26,13 @@ func TestStorageGroup(t *testing.T) {
 	sg.SetExpirationEpoch(exp)
 	require.Equal(t, exp, sg.ExpirationEpoch())
 
-	members := []*object.ID{objecttest.GenerateID(), objecttest.GenerateID()}
+	members := []*object.ID{objecttest.ID(), objecttest.ID()}
 	sg.SetMembers(members)
 	require.Equal(t, members, sg.Members())
 }
 
 func TestStorageGroupEncoding(t *testing.T) {
-	sg := storagegroup.New()
-	sg.SetValidationDataSize(13)
-	sg.SetValidationDataHash(testChecksum())
-	sg.SetExpirationEpoch(33)
-	sg.SetMembers([]*object.ID{objecttest.GenerateID(), objecttest.GenerateID()})
+	sg := storagegrouptest.Generate()
 
 	t.Run("binary", func(t *testing.T) {
 		data, err := sg.Marshal()
