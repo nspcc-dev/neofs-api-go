260cf35777b2aeb4368b99e934286f1b3a6b1627
Pavel Karpy <carpawell@nspcc.ru>
2021-06-08 21:37:20 +0300

    Bump SDK version in `pkg`
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/version.go
+++ pkg/version.go
@@ -9,7 +9,7 @@ import (
 // Version represents v2-compatible version.
 type Version refs.Version
 
-const sdkMjr, sdkMnr = 2, 6
+const sdkMjr, sdkMnr = 2, 7
 
 // NewVersionFromV2 wraps v2 Version message to Version.
 //
