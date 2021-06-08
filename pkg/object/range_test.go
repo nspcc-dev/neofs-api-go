96950250e0ba666a4ed2116673113467af3616ac
Pavel Karpy <carpawell@nspcc.ru>
2021-06-08 19:06:43 +0300

    [#302] pkg/object: Convert nil `Range` to nil message
    Document that `Range.ToV2` method return `nil`
    when called on `nil`. Document that `NewRangeFromV2`
    function return `nil` when called on `nil`. Write
    corresponding unit tests.
    
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/object/range_test.go
+++ pkg/object/range_test.go
@@ -1,6 +1,7 @@
 package object
 
 import (
+	"github.com/nspcc-dev/neofs-api-go/v2/object"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -22,4 +23,20 @@ func TestRange_SetLength(t *testing.T) {
 	r.SetLength(ln)
 
 	require.Equal(t, ln, r.GetLength())
+}
+
+func TestNewRangeFromV2(t *testing.T) {
+	t.Run("from nil", func(t *testing.T) {
+		var x *object.Range
+
+		require.Nil(t, NewRangeFromV2(x))
+	})
+}
+
+func TestRange_ToV2(t *testing.T) {
+	t.Run("nil", func(t *testing.T) {
+		var x *Range
+
+		require.Nil(t, x.ToV2())
+	})
 }
