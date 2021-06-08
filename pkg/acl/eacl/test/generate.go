88a3829b3917871c0bb0e6e7baa7387cc244bd24
Leonard Lyubich <leonard@nspcc.ru>
2021-06-08 11:18:36 +0300

    [#293] pkg/eacl: Implement and use generator of Target
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ pkg/acl/eacl/test/generate.go
@@ -0,0 +1,18 @@
+package eacltest
+
+import (
+	"github.com/nspcc-dev/neofs-api-go/pkg/acl/eacl"
+)
+
+// Target returns random eacl.Target.
+func Target() *eacl.Target {
+	x := eacl.NewTarget()
+
+	x.SetRole(eacl.RoleSystem)
+	x.SetBinaryKeys([][]byte{
+		{1, 2, 3},
+		{4, 5, 6},
+	})
+
+	return x
+}
