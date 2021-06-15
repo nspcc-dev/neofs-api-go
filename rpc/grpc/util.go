e459378b16ecc7f8c7f2e42b5a77b67e68e91cb8
Leonard Lyubich <leonard@nspcc.ru>
2021-06-15 10:08:31 +0300

    [#306] grpc: Fix the format of full method names
    All calling RPC's should have a leading slash according to docs of using
    gRPC library.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- rpc/grpc/util.go
+++ rpc/grpc/util.go
@@ -6,7 +6,7 @@ import (
 	"github.com/nspcc-dev/neofs-api-go/rpc/common"
 )
 
-const methodNameFmt = "%s/%s"
+const methodNameFmt = "/%s/%s"
 
 func toMethodName(p common.CallMethodInfo) string {
 	return fmt.Sprintf(methodNameFmt, p.Service, p.Name)
