7f339079c2e30e390263661df6dafda22b0513a3
Stanislav Bogatyrev <stanislav@nspcc.ru>
2021-08-05 09:18:19 +0300

    Fix well-known BasicACL constants to fit the spec
    In the well-known BasicACL constants we need to set the always toggled
    bits for the system group. Otherwise it may be confusing for those who
    read the specification and try to match it with the reference
    implementation or the resulting BasicACL set in the container.
    
    Signed-off-by: Stanislav Bogatyrev <stanislav@nspcc.ru>

--- pkg/acl/types.go
+++ pkg/acl/types.go
@@ -1,12 +1,12 @@
 package acl
 
 const (
-	// PublicBasicRule is a basic ACL value for public container.
-	PublicBasicRule = 0x1FFFFFFF
+	// PublicBasicRule is a basic ACL value for public-read-write container.
+	PublicBasicRule = 0x1FBFBFFF
 
 	// PrivateBasicRule is a basic ACL value for private container.
-	PrivateBasicRule = 0x18888888
+	PrivateBasicRule = 0x1C8C8CCC
 
-	// ReadOnlyBasicRule is a basic ACL value for read-only container.
-	ReadOnlyBasicRule = 0x1FFF88FF
+	// ReadOnlyBasicRule is a basic ACL value for public-read container.
+	ReadOnlyBasicRule = 0x1FBFBFFF
 )
