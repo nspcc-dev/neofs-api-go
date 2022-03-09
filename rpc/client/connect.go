50382114f40a1cc766f96dcdd03de685d5b98032
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-03-09 12:56:02 +0300

    [#383] rpc/client: Export URI-parsing function
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- rpc/client/connect.go
+++ rpc/client/connect.go
@@ -3,6 +3,9 @@ package client
 import (
 	"context"
 	"errors"
+	"fmt"
+	"net"
+	"net/url"
 
 	"github.com/nspcc-dev/neofs-api-go/v2/rpc/grpc"
 	grpcstd "google.golang.org/grpc"
@@ -51,4 +54,34 @@ func (c *Client) openGRPCConn() error {
 	cancel()
 
 	return err
+}
+
+// ParseURI parses s as address and returns a host and a flag
+// indicating that TLS is enabled. If multi-address is provided
+// the argument is returned unchanged.
+func ParseURI(s string) (string, bool, error) {
+	uri, err := url.ParseRequestURI(s)
+	if err != nil {
+		return s, false, nil
+	}
+
+	// check if passed string was parsed correctly
+	// URIs that do not start with a slash after the scheme are interpreted as:
+	// `scheme:opaque` => if `opaque` is not empty, then it is supposed that URI
+	// is in `host:port` format
+	if uri.Host == "" {
+		uri.Host = uri.Scheme
+		uri.Scheme = grpcScheme // assume GRPC by default
+		if uri.Opaque != "" {
+			uri.Host = net.JoinHostPort(uri.Host, uri.Opaque)
+		}
+	}
+
+	switch uri.Scheme {
+	case grpcTLSScheme, grpcScheme:
+	default:
+		return "", false, fmt.Errorf("unsupported scheme: %s", uri.Scheme)
+	}
+
+	return uri.Host, uri.Scheme == grpcTLSScheme, nil
 }
